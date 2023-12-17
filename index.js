ad2a6ef62100dba3d6693cbeb521f0d4dc44a913
enaprz <enasheccid7@gmail.com>
2023-12-16 20:56:05 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity= "roller-skate"){
+    
+    return `This Saturday, I want to ${activity}!`
+}
+// would be able to override by inserting diff argument when calling
+//ex. saturdayFun("study")
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork("work from home");
+
+function wrapAdjective(flair = "*"){
+    return function(adjective= "special"){
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+const encouragingPromptFunction = wrapAdjective("!!!");
+
+encouragingPromptFunction("hard worker");
+encouragingPromptFunction("||")("a dedicated programer")
